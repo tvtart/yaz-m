5908107d975d537d013e9ae62ce64825dbb5fe3a
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-15 12:39:48 +0300

    Add files via upload

---
+++ tvttest.php
@@ -0,0 +1,20 @@
+﻿<?php
+//Live video URL: https://abclive1-lh.akamaihd.net/i/abc_live01@423395/master.m3u8
+//Pre recorded video URL: http://qthttp.apple.com.edgesuite.net/1010qwoeiuryfg/sl.m3u8
+$url = $_GET['url'];
+if (!$url) {
+print "no URL";
+} 
+
+else {
+
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_HEADER, true);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$lastUrl = curl_getinfo($ch, CURLINFO_EFFECTIVE_URL);
+header('Content-Type: application/x-mpegurl'); 
+readfile($lastUrl); 
+curl_close($ch);
+}
+?>
