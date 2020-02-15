b2ba70a6337cc5c4187b218d67c91ddb70e9701d
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-15 12:39:28 +0300

    Delete tvttest.php

--- tvttest.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-$url = file_get_contents("http://yayin12.bunusanasoracagim.best:80/selcuksportsfutbol/eurosport22/playlist.m3u8");
-preg_match('#file:"(.*?)"#', $url,$getm3u8);
-
-echo ($getm3u8[1]);
-?>
