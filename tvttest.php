65ea57a77e2b273c17245f7531a31d1e620dcf6c
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-19 19:51:22 +0300

    Add files via upload

---
+++ tvttest.php
@@ -0,0 +1,11 @@
+﻿#EXTM3U
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS HABER" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS HABER
+http://iptvh.it/tvtsporhaber
+#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 1 SD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" grup başlığı = " TR SPOR ", TR: BEIN SPORTS 1 SD
+http://iptvh.it/tvtbeinspor1sd
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 HD
+http://yayin12.bunusanasoracagim.best:80/priv/b1111/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 HD
+https://makalemun.net:443/dd3_601.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 2 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hd.png" group-title="TR SPOR",TR:BEIN SPORTS 2 HD
+http://iptvh.it/tvtbeinspğr1hd
