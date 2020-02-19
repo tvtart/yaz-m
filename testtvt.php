a353698bd44789685b95b6e85527058c26042d59
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-19 22:10:53 +0300

    Delete testtvt.php

--- testtvt.php
+++
@@ -1,7 +0,0 @@
-﻿#EXTM3U
-#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS HABER" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS HABER
-http://iptvh.it/tvtsporhaber
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 1 SD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" grup başlığı = " TR SPOR ", TR: BEIN SPORTS 1 SD
-http://iptvh.it/tvtbeinspor1sd
-#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 2 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hd.png" group-title="TR SPOR",TR:BEIN SPORTS 2 HD
-http://iptvh.it/tvtbeinspğr1hd
