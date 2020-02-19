d9745c08fad50b2bf9aa4d8bd31835d5332e8ce8
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-19 19:46:43 +0300

    Add files via upload

---
+++ tvttest.php
@@ -0,0 +1,19 @@
+﻿#EXTM3U
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS HABER" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS HABER
+http://iptvh.it/tvtsporhaber
+#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 1 SD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" grup başlığı = " TR SPOR ", TR: BEIN SPORTS 1 SD
+http://iptvh.it/tvtbeinspor1sd
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 HD
+http://yayin12.bunusanasoracagim.best:80/priv/b1111/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 HD
+https://makalemun.net:443/dd3_601.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 2 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hd.png" group-title="TR SPOR",TR:BEIN SPORTS 2 HD
+http://iptvh.it/tvtbeinspğr1hd
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 3 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports3hd.png" group-title="TR SPOR",TR:BEIN SPORTS 3 HD
+http://yayin12.bunusanasoracagim.best:80/selcuksportsfutbol/bein3333/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 4 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports4hd.png" group-title="TR SPOR",TR:BEIN SPORTS 4 HD
+http://yayin12.bunusanasoracagim.best:80/selcuksportsfutbol/bein4444/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS MAX 1 UHD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1max.png" group-title="TR SPOR",TR:BEIN SPORTS MAX 1 UHD
+http://yayin12.bunusanasoracagim.best:80/selcuksportsfutbol/beinmax1111/playlist.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS MAX 2 UHD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hdmax.png" group-title="TR SPOR",TR:BEIN SPORTS MAX 2 UHD
+http://yayin12.bunusanasoracagim.best:80/selcuksportsfutbol/beinmax2222/playlist.m3u8
