dbca8af20d1632beab0211e713115b0f65276348
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-20 19:42:10 +0300

    Add files via upload

---
+++ testtvt1.php
@@ -0,0 +1,10 @@
+﻿<?php
+if(function_exists('file_get_contents')) {
+$data = file_get_contents('http://yayin12.bunusanasoracagim.best:80/selcuksportsfutbol/eurosport22/playlist.m3u8');
+preg_match('@<div class="burcDetail mBot30px"><p>(.*?)</div>@si', $data, $content);
+echo $content[0];
+} else {
+echo "wm araci";
+}
+?>
+<?php echo $parts[0]; ?>
