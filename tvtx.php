56c6e21c396d005738ef5d4db2440f78dbbf4c0d
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-22 14:19:51 +0300

    Delete tvtx.php

--- tvtx.php
+++
@@ -1,9 +0,0 @@
-﻿#EXTM3U
-#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 HD
-https://1xstream.rakip1.tv:443/beinsport1/video.m3u8
-#EXTINF:-1 tvg-id="" tvg-name="TR: BEIN SPORTS 1 HD" tvg-logo="http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" group-title="TR SPOR",TR:BEIN SPORTS 1 HD
-https://makalemun.net:443/dd3_601.m3u8
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 2 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 2 HD
-https://1xstream.rakip1.tv:443/beinsport2/video.m3u8
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 3 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports3hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 3 HD
-https://1xstream.rakip1.tv:443/beinsport3/video.m3u8
