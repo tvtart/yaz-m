7af1c38e1a247cfc286ce704e522607837a83eb7
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-22 20:10:42 +0300

    Add files via upload

---
+++ tvtx.php
@@ -0,0 +1,36 @@
+﻿#EXTM3U
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS HABER
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1436.m3u8
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" grup başlığı = " TR SPOR ", TR: BEIN SPORTS 1 HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1429.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS 1 UHD
+
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1430.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS 2 HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1432.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS 3 HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1433.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS 4 HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1434.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS MAX 1 UHD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1437.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:BEIN SPORTS MAX 2 UHD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1438.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:TIVIBUSPOR 1 UHD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1441.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:TIVIBUSPOR 2 UHD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1442.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:TIVIBUSPOR 3 UHD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1443.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:TIVIBUSPOR 4 UHD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1444.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:S SPORT 1 HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1451.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:S SPORT 2 HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1452.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:SMART SPOR  HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1453.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:EUROSPORT 1 HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1461.m3u8
+#EXTINF:-1 tvg-id="" tvg-name="" tvg-logo="" group-title="TR SPOR",TR:EUROSPORT 2 HD
+http://apptv.iptvlg.com:8080/live/asasdad/adadad/1462.m3u8
