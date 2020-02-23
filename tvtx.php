dd1934bd8ae824124762c503b59112275b3cae0e
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-23 20:47:04 +0300

    Update tvtx.php

--- tvtx.php
+++ tvtx.php
@@ -2,14 +2,4 @@
 #EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 1 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 1 HD
 https://cstream12.com:443/hls/beinsports1_tr_althd.m3u8
 #EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 1 SD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 1 SD
-https://cstream12.com:443/hls/beinsports1_tr_hd.m3u8
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 2 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 2 HD
-https://cstream12.com:443/hls/beinsports2_tr_si.m3u8
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 3 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports3hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 3 HD
-https://cstream12.com:443/hls/beinsports3_tr_si.m3u8
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 4 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports4hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 4 HD
-https://cstream12.com:443/hls/beinsports4_tr_si.m3u8
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS MAX 1 UHD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1max.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS MAX 1 UHD
-https://cstream12.com:443/hls/beinsports1max_tr_si.m3u8
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS MAX 2 UHD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hdmax.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS MAX 2 UHD
-https://cstream12.com:443/hls/beinsports2max_tr_si.m3u8
+https://cstream12.com:443/hls/beinsports1_tr_althd.m3u8
