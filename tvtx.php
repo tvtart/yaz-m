2824a6ac8bb2142bc284b83d3457a68f107f04de
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-23 20:06:24 +0300

    Update tvtx.php

--- tvtx.php
+++ tvtx.php
@@ -1,3 +1,7 @@
 ﻿#EXTM3U
 #EXTINF:-1,TR | BEIN SPORTS 1 HD
 https://cstream12.com:443/hls/beinsports1_tr_fhd.m3u8
+#EXTINF:-1,TR | BEIN SPORTS 2 HD
+https://cstream12.com:443/hls/beinsports2_tr_fhd.m3u8
+#EXTINF:-1,TR | BEIN SPORTS 3 HD
+https://cstream12.com:443/hls/beinsports3_tr_fhd.m3u8
