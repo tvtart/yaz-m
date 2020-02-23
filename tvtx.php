b8ef8f4989edb4de606804b13556e457ec346a52
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-23 20:44:32 +0300

    Update tvtx.php

--- tvtx.php
+++ tvtx.php
@@ -1,6 +1,6 @@
 ﻿#EXTM3U
 #EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 1 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 1 HD
-https://cstream12.com:443/hls/beinsports1_tr_uhd.m3u8
+https://cstream12.com:443/hls/beinsports1_tr_althd.m3u8
 #EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 1 SD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 1 SD
 https://cstream12.com:443/hls/beinsports1_tr_hd.m3u8
 #EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 2 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports2hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 2 HD
