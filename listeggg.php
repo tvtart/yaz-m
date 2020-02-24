01ea06d77151e7abae49dcc4e48e0053df1db4e8
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-24 17:01:37 +0300

    Update listeggg.php

--- listeggg.php
+++ listeggg.php
@@ -1,34 +1,11 @@
-{
-   " list " : {
-     " service " : " iptv " ,
-     " title " : " iptv " ,
-     " item " : [
-      {
-        " service " : " iptv " ,
-         " title " : " Rrokum " ,
-         " playlistURL " : " https://raw.githubusercontent.com/onigetoc/m3u8-PHP-Parser/master/ressources/demofile.m3u " ,
-         " media_url " : " http://82.114.65.202:1935/live/rrokumtv/playlist.m3u8 " ,
-         " url " : " http://82.114.65.202:1935/live/rrokumtv/playlist.m3u8 " ,
-         "id " : " RrokumTV.al " ,
-         " en boy oranı " : " 16: 9 " ,
-         " yazar " : " RrokumTV.al " ,
-         " dil " : " Arnavutça / Kosova " ,
-         " ülke " : " KS " ,
-         " thumb_square " : "https://2.bp.blogspot.com/-adXhlGiHqxw/WSGCBl9Y8LI/AAAAAAAAD5Q/-t48sLv989Uxb5hgB0b38QlfTdoAE576ACLcB/s1600/tvrrokum.png " " grup " : " TVSHQIP "
-      },
-     {
-        " service " : " iptv " ,
-         " title " : " JTV " ,
-         " playlistURL " : " https://raw.githubusercontent.com/onigetoc/m3u8-PHP-Parser/master/ressources/demofile.m3u " ,
-         " media_url " : " http://84.20.77.50:554/live/rtmp.stream/playlist.m3u8 " ,
-         " url " : " http://84.20.77.50:554/live/rtmp.stream/playlist.m3u8 " ,
-         "id " : " TVJUG.AL " ,
-         " yazar " : " TVJUG.AL " ,
-         " dil " : " Arnavutça / Kosova " ,
-         " ülke " : " AL " ,
-         " thumb_square " : " https: //4.bp. blogspot.com/-LXFMStwz6tI/WSGLzk0QgVI/AAAAAAAAD6I/T-rFp4KD_c4zbCRN9hGrjy_jS2nmXgPJwCLcB/s1600/tvjug.png " ,
-         "grup " : "TVSHQIP "
-      }
-    ]
-  }
-}
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<p><button type="button" style="background-color: #000000;"><a href="https://cstream12.com:443/hls/beinsports1_tr_si.m3u8" style="color: #ffffff;">BEİN SPOR 1</a></button></p>
+<p><button type="button" style="background-color: #000000;"><a href="https://cstream12.com:443/hls/beinsports2_tr_si.m3u8" style="color: #ffffff;">BEİN SPOR 2</a></button></p>
+<p> <button type="button" style="background-color: #000000;"><a href="https://cstream12.com:443/hls/beinsports3_tr_si.m3u8" style="color: #ffffff;">BEİN SPOR 3</a></button></p>
+<p><button type="button" style="background-color: #000000;"><a href="https://cstream12.com:443/hls/beinsports4_tr_si.m3u8" style="color: #ffffff;">BEİN SPOR 4</a></button></p>
+<p><button type="button" style="background-color: #000000;"><a href="https://cstream12.com:443/hls/beinsports1max_tr_si.m3u8" style="color: #ffffff;">BEİN MAX 1 SPOR </a></button></p>
+<p><button type="button" style="background-color: #000000;"><a href="https://cstream12.com:443/hls/beinsports2max_tr_si.m3u8" style="color: #ffffff;">BEİN MAX 2 SPOR </a></button></p>
