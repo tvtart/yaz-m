eaee90f7c265d41794f2ae40c15bfb653c940874
tvtart <60048999+tvtart@users.noreply.github.com>
2020-02-24 13:28:59 +0300

    Update listeggg.php

--- listeggg.php
+++ listeggg.php
@@ -1,27 +1,34 @@
-<?php
-
-$curl = curl_init('http://yayin12.bunusanasoracagim.best:80/priv/b1111/playlist.m3u8');
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
-
-$page = curl_exec($curl);
-$stream_name = "Webcam";
-
-if(curl_errno($curl)) // check for execution errors
 {
-    echo 'Scraper error: ' . curl_error($curl);
-    exit;
+   " list " : {
+     " service " : " iptv " ,
+     " title " : " iptv " ,
+     " item " : [
+      {
+        " service " : " iptv " ,
+         " title " : " Rrokum " ,
+         " playlistURL " : " https://raw.githubusercontent.com/onigetoc/m3u8-PHP-Parser/master/ressources/demofile.m3u " ,
+         " media_url " : " http://82.114.65.202:1935/live/rrokumtv/playlist.m3u8 " ,
+         " url " : " http://82.114.65.202:1935/live/rrokumtv/playlist.m3u8 " ,
+         "id " : " RrokumTV.al " ,
+         " en boy oranı " : " 16: 9 " ,
+         " yazar " : " RrokumTV.al " ,
+         " dil " : " Arnavutça / Kosova " ,
+         " ülke " : " KS " ,
+         " thumb_square " : "https://2.bp.blogspot.com/-adXhlGiHqxw/WSGCBl9Y8LI/AAAAAAAAD5Q/-t48sLv989Uxb5hgB0b38QlfTdoAE576ACLcB/s1600/tvrrokum.png " " grup " : " TVSHQIP "
+      },
+     {
+        " service " : " iptv " ,
+         " title " : " JTV " ,
+         " playlistURL " : " https://raw.githubusercontent.com/onigetoc/m3u8-PHP-Parser/master/ressources/demofile.m3u " ,
+         " media_url " : " http://84.20.77.50:554/live/rtmp.stream/playlist.m3u8 " ,
+         " url " : " http://84.20.77.50:554/live/rtmp.stream/playlist.m3u8 " ,
+         "id " : " TVJUG.AL " ,
+         " yazar " : " TVJUG.AL " ,
+         " dil " : " Arnavutça / Kosova " ,
+         " ülke " : " AL " ,
+         " thumb_square " : " https: //4.bp. blogspot.com/-LXFMStwz6tI/WSGLzk0QgVI/AAAAAAAAD6I/T-rFp4KD_c4zbCRN9hGrjy_jS2nmXgPJwCLcB/s1600/tvjug.png " ,
+         "grup " : "TVSHQIP "
+      }
+    ]
+  }
 }
-
-curl_close($curl);
-
-$regex = '/token=(.*?)"/';
-if ( preg_match($regex, $page, $match) )
-    $stream_url =  "http://yayin12.bunusanasoracagim.best:80/priv/b1111/playlist.m3u8".$match[1];
-else ($stream_url =  "error");
-
-header('Content-Type: application/octet-stream');
-echo "#EXTM3U\n";
-echo "#EXTINF:-1," . $stream_name . "\n";
-echo $stream_url;
-
-?>
