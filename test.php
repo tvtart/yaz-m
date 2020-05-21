a6c7a01a14f2d781be1ec0135aca4aac190c0b01
tvtart <60048999+tvtart@users.noreply.github.com>
2020-05-21 12:38:24 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,2 +1,1 @@
-#EXTINF: -1 tvg-id = "" tvg-name = "TR: BEIN SPORTS 1 HD" tvg-logo = "http://epgserver.sihirmedya.com/logoslar/tr/beinsports1hd.png" grup başlığı = "TR SPOR", TR: BEIN SPORTS 1 HD
 http://newshqip.dyndns.tv:8080/live/extreamnew123456/ansjahanshsksn78jshn/8268.m3u8
