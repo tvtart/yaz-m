80d8435f400d25c225ec40ce0a18c1873e6c29aa
tvtart <60048999+tvtart@users.noreply.github.com>
2020-05-21 12:30:05 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$url = file_get_contents("https://cdn.nsxmedia.net:443/hlspubx/143.m3u8");
+$url = file_get_contents("https://ubld.cc/78y4e");
 preg_match('#file:"(.*?)"#', $url,$getm3u8);
 
 echo ($getm3u8[1]);
