197286303884e70dffa409abc59b560624c033b3
tvtart <60048999+tvtart@users.noreply.github.com>
2020-08-06 21:36:36 +0300

    Add files via upload

---
+++ 77.php
@@ -0,0 +1,10 @@
+﻿{
+    "startTime": 1537444800,
+    "dvrWindowStartTime": 1537444800,
+    "dvrWindowEndTime": 1537448400,
+    "dataProvider": {
+        "source": [
+            { "url": "https://vizyontv.yayin.com.tr/vizyontv/vizyontv_720p/playlist.m3u8" }
+        ]
+    }
+}
