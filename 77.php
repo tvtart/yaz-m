82775ee2ecfc9fb699d8d0c7758a73e80adf2275
tvtart <60048999+tvtart@users.noreply.github.com>
2020-08-06 21:43:44 +0300

    Add files via upload

---
+++ 77.php
@@ -0,0 +1,13 @@
+﻿<link rel="stylesheet" href="http://releases.flowplayer.org/7.0.2/skin/skin.css">
+<style>.flowplayer {background-color: #00abcd;}
+.flowplayer .fp-color-play {fill: #eee;}
+</style>
+<script src="http://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="http://releases.flowplayer.org/7.0.2/flowplayer.min.js"></script>
+<script src="http://releases.flowplayer.org/hlsjs/flowplayer.hlsjs.min.js"></script>
+<div data-live="true" data-ratio="0.5625" class="flowplayer">
+<video data-title="Live stream">
+<source type="application/x-mpegurl"
+src="https://vizyontv.yayin.com.tr/vizyontv/vizyontv_720p/playlist.m3u8">
+</video>
+</div>
