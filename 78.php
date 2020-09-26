8ff43ece4ecd236873b9f9bbfce060f200546ef2
tvtart <60048999+tvtart@users.noreply.github.com>
2020-09-26 21:50:32 +0300

    Add files via upload

---
+++ 78.php
@@ -0,0 +1,27 @@
+#EXTM3U
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES ACTION
+https://egoldstudio.com/eal/tvt/salon1.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES ACTION 2
+https://egoldstudio.com/eal/tvt/salon2.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES TURK
+https://egoldstudio.com/eal/tvt/salon3.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES PREMIERE
+https://egoldstudio.com/eal/tvt/salon4.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES PREMIERE 2
+https://egoldstudio.com/eal/tvt/salon5.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES STARS
+https://egoldstudio.com/eal/tvt/salon6.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES FAMLY
+https://egoldstudio.com/eal/tvt/salon7.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", MOVIE SMART ACTION
+https://egoldstudio.com/eal/tvt/salon8.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", MOVIE SMART PLATIN 2
+https://egoldstudio.com/eal/tvt/salon9.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", MOVIE SMART PRENIUM
+https://egoldstudio.com/eal/tvt/salon10.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", MOVIE SMART PRENIUM 2
+https://egoldstudio.com/eal/tvt/salon11.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", MOVIE SMART GOLD
+https://egoldstudio.com/eal/tvt/salon12.php
+#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", MOVIE SMART FAMLY
+https://egoldstudio.com/eal/tvt/salon13.php
