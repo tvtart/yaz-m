2e662d11c6496826d47b0f09dfe92e4a4171ae71
tvtart <60048999+tvtart@users.noreply.github.com>
2020-12-05 16:47:17 +0300

    Update 78.php

--- 78.php
+++ 78.php
@@ -1,3 +1,3 @@
 #EXTM3U
 #EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES ACTION
-https://x.ppsiktibeniartik.xyz:443/kaynakstreamradarco/13/naxi/caxi.m3u8
+https://srvc.bengormemruyaa.xyz:443/kaynakstreamradarco/selcukbein1app/strmrdr.m3u8
