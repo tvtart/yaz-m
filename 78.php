32fe32f05747a4bb6e2cc53272311025dd80fa6c
tvtart <60048999+tvtart@users.noreply.github.com>
2020-12-05 19:29:22 +0300

    Update 78.php

--- 78.php
+++ 78.php
@@ -1,3 +1,3 @@
 #EXTM3U
 #EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES ACTION
-http://ssl.p.jwpcdn.com/player/v/8.18.2/jwplayer.flash.swf
+http://cdn.jwplayer.com/v2/media/aVBbz386.m3u8
