7491e34bbf02a3e596816b7bd551251815e0a404
tvtart <60048999+tvtart@users.noreply.github.com>
2020-12-05 19:52:24 +0300

    Update 78.php

--- 78.php
+++ 78.php
@@ -1,3 +1,3 @@
 #EXTM3U
 #EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES ACTION
-https://content.jwplatform.com/videos/aVBbz386-640.mp4
+https://content.jwplatform.com/videos/aVBbz386.m3u8
