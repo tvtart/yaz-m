519e7a0da845df65160b017ad1cf6471d7ea2156
tvtart <60048999+tvtart@users.noreply.github.com>
2020-12-07 22:02:40 +0300

    Update 79.php

--- 79.php
+++ 79.php
@@ -1,23 +1,4 @@
 #EXTM3U
-#EXTINF:-1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", BEİN SPOR HABER
-https://egoldstudio.com/eal/77.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", BEİN SPOR 1
-https://egoldstudio.com/eal/1.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", BEİN SPOR 2
-https://egoldstudio.com/eal/2.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", BEİN SPOR 3
-https://egoldstudio.com/eal/3.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", BEİN SPOR 4
-https://egoldstudio.com/eal/4.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", BEİN NAX 1
-https://egoldstudio.com/eal/5 php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", BEİN NAX 2
-https://egoldstudio.com/eal/6.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", EUROSPOR 1
-https://egoldstudio.com/eal/7.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", EUROSPOR 2
-https://egoldstudio.com/eal/8.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", SSPOR
-https://egoldstudio.com/eal/9.php
-#EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SPOR", SMART SPOR
-https://egoldstudio.com/eal/10.php
+#EXT-X-VERSION:3
+#EXT-X-STREAM-INF:BANDWIDTH=5511686,RESOLUTION=1920x1080,CODECS="avc1.42c028,mp4a.40.2"
+naxi/caxi.m3u8
