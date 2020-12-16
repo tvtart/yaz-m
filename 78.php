34cc9582270ddc848afed2b366904c10aa23cb71
tvtart <60048999+tvtart@users.noreply.github.com>
2020-12-16 14:23:57 +0300

    Update 78.php

--- 78.php
+++ 78.php
@@ -1,3 +1,3 @@
 #EXTM3U
 #EXTINF: -1 tvg-id = "" tvg-name = "" tvg-logo = "" group-title = "SİNEMA", BEIN MOVIES ACTION
-https://egoldstudio.com/eal/80.php
+https://s3.1legal.cloud/bimba27/index.m3u8
